45b80600e9352b8f77f198df407bd20ec08b1df7
manhun97 <x01643353354x@gmail.com>
2019-05-13 18:03:31 +0700

    Update

--- app/src/main/java/com/dino/kqxs/MainActivity.java
+++ app/src/main/java/com/dino/kqxs/MainActivity.java
@@ -1,16 +1,26 @@
 package com.dino.kqxs;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import com.dino.kqxs.fragments.DashboardFragment;
 import com.dino.kqxs.fragments.HomeFragment;
 import com.dino.kqxs.fragments.NotificationsFragment;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,6 +59,22 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(new HomeFragment());
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void loadViewModel(ViewModel viewModel) throws IOException {
+        Document doc = Jsoup.connect("https://ketqua.net/").get();
+        TextView textview = findViewById(R.id.textViewhtml);
+        textview.setText("");
+        String html = "";
+        for (Element element : doc.select("result_tab_mb")) {
+            if (element.hasText()) {
+                html = html + element.text();
+                break;
+            }
+        }
+        System.out.println(html);
+        textview.setText(Html.fromHtml(html));
+        //textview.setText(html);
     }
     private void loadFragment(Fragment fragment) {
         // load fragment
