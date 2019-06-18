77312f35311df0fcac47b2140e45ebbc3b183e7e
manhun97 <x01643353354x@gmail.com>
2019-06-18 22:25:43 +0700

    Update live kqxs

--- app/src/main/java/com/dino/kqxs/fragments/HomeFragment.java
+++ app/src/main/java/com/dino/kqxs/fragments/HomeFragment.java
@@ -5,31 +5,34 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.dino.kqxs.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel mViewModel;
-
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
-    }
-
+    public WebView mWebView;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
+        mWebView = v.findViewById(R.id.web);
+        mWebView.loadUrl("http://192.168.1.152/kqxs/kqmb.html");
 
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        return v;
+    }
 }
--- app/src/main/java/com/dino/kqxs/fragments/HomeViewModel.java
+++ app/src/main/java/com/dino/kqxs/fragments/HomeViewModel.java
@@ -4,4 +4,5 @@ import android.arch.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+
 }
